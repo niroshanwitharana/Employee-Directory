cad8fe0653a7985f466d4f0cc2c68c5471055cf2
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-08-09 01:33:27 +0100

    header created

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className= 'header text-primary py-5'>
+            <h1>Employee Directory</h1>
+        </div>
+    )
+}
+
+export default Header;
